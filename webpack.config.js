41568752cd0102d522415ff07dd0af61b3736fa2
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-05-19 12:22:49 -0700

    Added Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -19,7 +24,51 @@ module.exports = {
                     loader: "babel-loader", // specify the loader
                 },
             },
-            
-        ]
-    }
-}
+            // SCSS to CSS
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            // images into ./dist
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8000,//Convert images < 8kb to base64 strings
+                            name: "images/[hash]-[name].[ext]",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    // minify CSS
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin()],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "./dist"),
+        open: true,
+        compress: true,
+        hot: true,
+        watchOptions: {
+            poll: true,
+            ignored: "/node_modules/",
+        },
+        port: 8080
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].css"}),
+        new HtmlWebpackPlugin({
+            title: "React Project",
+            template: "./src/index.html",
+            cache: false,
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
