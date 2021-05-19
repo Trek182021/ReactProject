571f80dd80ab758655d025a3f9fdba90580516f6
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-05-19 11:49:21 -0700

    Add

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "./",
+        assetModuleFilename: "images/[name][ext][query]",
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // using regex to tell babel exactly what files to transpile
+                exclude: /node_modules/, //files to ignore
+                use: {
+                    loader: "babel-loader", // specify the loader
+                }
+            }
+        ]
+    }
+}
